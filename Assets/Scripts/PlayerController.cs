ae4d5bd7fd2a12c5bb94090a69d18d9a0c2e6f88
Komodeo <onlythebest@gmail.com>
2020-07-28 15:01:37 -0400

    scrolling improvements and game over
    - background scrolls by infinitely
    - objects destroyed when they leave the screen
    - game over (when an obstacle is hit):
    -- screen stops scrolling
    -- objects stop spawning
    -- game over text displays in console

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityModifier = 1f;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +28,15 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
-        isOnGround = true;
+    { 
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
