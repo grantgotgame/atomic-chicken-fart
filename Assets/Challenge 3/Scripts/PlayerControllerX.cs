07a222aa4b65949bb866350c4a84f63f172b91d9
Komodeo <onlythebest@gmail.com>
2020-07-29 18:30:19 -0400

    complete challenges

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public bool gameOver;
+    public bool gameOver = false;
 
     public float floatForce;
     private float gravityModifier = 1.5f;
+    private float topBound = 14.4f;
+    private float bounceForce = 700f;
+    private float sceneTimer = 5f;
+
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -16,27 +21,42 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
-
+    public AudioClip bounceSound;
 
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-
     }
 
     // Update is called once per frame
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && transform.position.y < topBound && playerRb.velocity.y < floatForce)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
+        else if (transform.position.y > topBound && playerRb.velocity.y > 0)
+        {
+            playerRb.AddForce(Vector3.up * -floatForce);
+        }
+
+        // Restart game after scene timer expires on game over
+        if (gameOver == true)
+        {
+            sceneTimer -= Time.deltaTime;
+            if (sceneTimer < 0)
+            {
+                SceneManager.LoadScene("Challenge 3");
+            }
+        }
+        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -49,17 +69,27 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
+            gameObject.transform.localScale = new Vector3(0, 0, 0);
+        }        
 
+        // if player collides with ground, bounce back and play sound
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * bounceForce);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         // if player collides with money, fireworks
-        else if (other.gameObject.CompareTag("Money"))
+        if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
         }
-
     }
 
 }
